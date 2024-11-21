abeea6673e3969249e9b682f2e1425734a21d705
Scappodinotte <elia.scapini@gmail.com>
2024-11-21 17:53:26 +0100

    Add code file

---
+++ code_econometrics.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(boot)
+library(ISLR2)
+library(leaps)
+library(Matrix)
+library(glmnet)
+library(car)
+
+
+# Delete all objects in the memory
+rm(list=ls())
+
+# Create results folder
+setwd("~/Personale/UNINE/Master_Applied_Economics/Sem1/Econometrics/Case_study")
+
+data <- read.csv("fredgraph.csv", sep = ",", header = T)
+time <- data$Date
+data <- data[, -1]
+head(data)
+
+plot(data)
+# Outlier for MABSE due to exchange rate intervention 2011
+
+model <- lm(CPI ~ ., data = data)
+summary(model)
+
